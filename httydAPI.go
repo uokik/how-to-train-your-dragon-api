b6a609619fc7c106859da87c4303f5365bdeb33f
szok <wtfszok1337@gmail.com>
2025-11-17 14:30:08 +0100

    added getdragonbyclass

--- httydAPI.go
+++ httydAPI.go
@@ -3,10 +3,11 @@ package httydAPI
 import "github.com/szzok/how-to-train-your-dragon-api/service"
 
 var (
-	GetAllVikings   = service.GetAllVikings
-	GetVikingByID   = service.GetVikingByID
-	GetAllDragons   = service.GetAllDragons
-	GetDragonByID   = service.GetDragonByID
-	GetAllLocations = service.GetAllLocations
-	GetLocationByID = service.GetLocationByID
+	GetAllVikings     = service.GetAllVikings
+	GetVikingByID     = service.GetVikingByID
+	GetAllDragons     = service.GetAllDragons
+	GetDragonByID     = service.GetDragonByID
+	GetAllLocations   = service.GetAllLocations
+	GetLocationByID   = service.GetLocationByID
+	GetDragonsByClass = service.GetDragonsByClass
 )
